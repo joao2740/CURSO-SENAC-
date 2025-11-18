b045f847791081f098b24bdcd08ac2108700980d
JOAO VITOR <149392154+joao2740@users.noreply.github.com>
2025-11-18 17:33:31 -0300

    Add files via upload

---
+++ UC7/12. Atividade Prática Aprimoramento da API RESTful de Usuários/db.js
@@ -0,0 +1,28 @@
+// db.js
+
+const mysql = require('mysql2/promise');
+
+const dbConfig = {
+    host: 'localhost',      
+    user: 'root',           
+    password: 'senac',           
+    database: 'api_usuarios', 
+    port: 3307, 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0 
+};
+
+const pool = mysql.createPool(dbConfig);
+
+pool.getConnection()
+    .then(connection => {
+        console.log("Conexão com MySQL estabelecida com sucesso! ");
+        connection.release(); 
+    })
+    .catch(e => {
+        console.error("ERRO: Falha ao conectar ao MySQL. Verifique o XAMPP e as credenciais.");
+        console.error(e.message);
+    });
+
+module.exports = pool;
